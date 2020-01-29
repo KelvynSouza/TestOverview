46f04ccde8e2709bd44809cfd34df890c27e1f58
Kelvyn <KelvynSouza>
2020-01-29 18:58:52 -0300

    coisas

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -20,6 +20,18 @@ export class NavbarComponent implements OnInit {
           this.sidebarVisible = false;
     }
 
+    fileContent: string = '';
+
+    public onChange(fileList: FileList): void {
+      let file = fileList[0];
+      let fileReader: FileReader = new FileReader();
+      let self = this;
+      fileReader.onloadend = function(x) {
+        self.fileContent = fileReader.result;
+      }
+      fileReader.readAsText(file);
+    }
+
     ngOnInit(){
       this.listTitles = ROUTES.filter(listTitle => listTitle);
       const navbar: HTMLElement = this.element.nativeElement;
